9eef51e040489d58a1604d1272582186da71431f
Babalo Majiyezi <raymondbabalo@gmail.com>
2023-08-08 13:56:57 +0200

    added ui to the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock, Paper Scissors
+
+A simple Rock Paper Scissors game implemented in JavaScript. Play against the computer and see if you can win!
--- script.js
+++ script.js
@@ -1,40 +1,86 @@
+let userScore = 0;
+let computerScore = 0;
+const gameRounds = 5;
+let attempts = 0;
+
+// Get elements from the DOM
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const resultDisplay = document.getElementById("result");
+const scoreDisplay = document.getElementById("score");
+const body = document.querySelector('body');
+
+// Function to generate computer choice
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
-function getPlayerChoice() {
-    let playerOption = prompt("Choose: 'rock', 'paper', 'scissors' ").toLowerCase();
-    return playerOption;
-}
-
-function playRound(playerSelection, computerSelection) {
-    // GAME LOGIC
-    if (playerSelection === computerSelection) {
-        console.log("It's a Tie! ");
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(`You win! The computer chose ${computerSelection} and you chose ${playerSelection}`);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(`You win! The computer chose ${computerSelection} and you chose ${playerSelection}`);
-    } else if (playerSelection === "scissors" && computerSelection == "paper") {
-        console.log(`You win! The computer chose ${computerSelection} and you chose ${playerSelection}`);
+// Function to determine the winner
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It's a tie !";
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        userScore += 1;
+        return "You win!";
     } else {
-        console.log(`You lose! The computer chose ${computerSelection} and you chose ${playerSelection}`);
+        computerScore += 1;
+        return "Computer wins!";
     }
 }
 
-function game() {
-    let attempts = 0;
-    let rounds = 5;
-    for (let i = 0; i < rounds; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+// Event listeners for user choices
+rockButton.addEventListener("click", playRound);
+
+paperButton.addEventListener("click", playRound);
+
+scissorsButton.addEventListener("click", playRound);
+
+function updateScoreDisplay() {
+    return scoreDisplay.textContent = `${userScore} - ${computerScore}`;
+}
+
+function playRound(event) {
+    if (attempts < gameRounds) {
+        const userChoice = event.target.id;
+        const computerChoice = getComputerChoice();
+        const result = determineWinner(userChoice, computerChoice);
+        resultDisplay.textContent = `You chose ${userChoice} and the computer chose ${computerChoice}. ${result}`;
+        resultDisplay.style.cssText = "text-transform: uppercase;"
+
         attempts += 1;
+        updateScoreDisplay();
+
+        if (attempts === gameRounds) {
+            announceWinner();
+        }
     }
 }
 
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(game());
+function announceWinner() {
+    let winner;
+    if (userScore > computerScore) {
+        winner = "You win!";
+    } else if (userScore < computerScore) {
+        winner = "Computer wins";
+    } else {
+        winner = "It's a tie";
+    }
+
+    resultDisplay.textContent = `Game over! ${winner}!`;
+
+    const message = document.createElement('p');
+    message.textContent = "Hit the refresh button or F5 to play again !";
+    message.style.cssText = "text-transform: uppercase;";
+    body.append(message);
+
+}
+
+updateScoreDisplay();
